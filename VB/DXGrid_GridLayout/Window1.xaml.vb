661fe4b8be5fd342a1fb1ca401a8e32fc48c2fb2
DevExpressExampleBot <fakeEmail>
2022-06-08 18:34:35 +0400

    Source auto update [skip ci]

--- VB/DXGrid_GridLayout/Window1.xaml.vb
+++ VB/DXGrid_GridLayout/Window1.xaml.vb
@@ -1,13 +1,67 @@
-﻿Imports System.Collections.Generic
+Imports System.Collections.ObjectModel
+Imports System.IO
 Imports System.Windows
-Imports System.Windows.Documents
 
 Namespace DXGrid_GridLayout
-    Partial Public Class Window1
+
+    Public Partial Class Window1
         Inherits Window
 
+        Public Shared ReadOnly IsLayoutSavedProperty As DependencyProperty = DependencyProperty.Register("IsLayoutSaved", GetType(Boolean), GetType(Window1), Nothing)
+
+        Public Property IsLayoutSaved As Boolean
+            Get
+                Return CBool(GetValue(IsLayoutSavedProperty))
+            End Get
+
+            Set(ByVal value As Boolean)
+                SetValue(IsLayoutSavedProperty, value)
+            End Set
+        End Property
+
+        Private layoutStream As MemoryStream
+
         Public Sub New()
-            InitializeComponent()
+            DataContext = Me
+            Me.InitializeComponent()
+            IsLayoutSaved = False
+            Me.grid.ItemsSource = IssueList.GetData()
         End Sub
+
+        Private Sub OnSaveLayout(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            layoutStream = New MemoryStream()
+            Me.grid.SaveLayoutToStream(layoutStream)
+            IsLayoutSaved = True
+        End Sub
+
+        Private Sub OnRestoreLayout(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            layoutStream.Position = 0
+            Me.grid.RestoreLayoutFromStream(layoutStream)
+        End Sub
+
+        Private Sub OnAddNewColumn(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Me.grid.Columns.Add(New DevExpress.Xpf.Grid.GridColumn() With {.FieldName = "IsPrivate"})
+        End Sub
+    End Class
+
+    Public Class IssueList
+
+        Shared Public Function GetData() As ObservableCollection(Of IssueDataObject)
+            Dim data As ObservableCollection(Of IssueDataObject) = New ObservableCollection(Of IssueDataObject)()
+            data.Add(New IssueDataObject() With {.IssueName = "Transaction History", .IssueType = "Bug", .IsPrivate = True})
+            data.Add(New IssueDataObject() With {.IssueName = "Ledger: Inconsistency", .IssueType = "Bug", .IsPrivate = False})
+            data.Add(New IssueDataObject() With {.IssueName = "Data Import", .IssueType = "Request", .IsPrivate = False})
+            data.Add(New IssueDataObject() With {.IssueName = "Data Archiving", .IssueType = "Request", .IsPrivate = True})
+            Return data
+        End Function
+    End Class
+
+    Public Class IssueDataObject
+
+        Public Property IssueName As String
+
+        Public Property IssueType As String
+
+        Public Property IsPrivate As Boolean
     End Class
 End Namespace
